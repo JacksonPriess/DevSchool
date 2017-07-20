7f3cc0e149ca08e6e6148dd26f66bf8faff736b8
jackson <jacksonpriess@gmail.com>
2017-07-20 01:04:32 -0300

    Melhorando palavras

--- DevSchool/Models/Task.cs
+++ DevSchool/Models/Task.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,12 +10,16 @@ namespace DevSchool.Models
     {
         public int Id { get; set; }
 
+        [Display(Name = "Título")]
         public String Title { get; set; }
 
+        [Display(Name = "Descrição")]
         public String Description { get; set; }
 
+        [Display(Name = "Completa")]
         public bool Status { get; set; }
 
+        [Display(Name = "Criação")]
         public DateTime CreationDate { get; set; }
     }
     
