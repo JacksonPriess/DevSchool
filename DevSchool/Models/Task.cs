788fa55ebdb9ff4694ec9fd9c58c8f8cfae810c8
jackson <jacksonpriess@gmail.com>
2017-07-20 15:24:24 -0300

    Adicionado validação para data de criação, título e descrição

--- DevSchool/Models/Task.cs
+++ DevSchool/Models/Task.cs
@@ -11,15 +11,18 @@ namespace DevSchool.Models
         public int Id { get; set; }
 
         [Display(Name = "Título")]
+        [Required]
         public String Title { get; set; }
 
         [Display(Name = "Descrição")]
+        [Required]
         public String Description { get; set; }
 
         [Display(Name = "Completa")]
         public bool Status { get; set; }
 
         [Display(Name = "Criação")]
+        [DataType(DataType.Date)]
         public DateTime CreationDate { get; set; }
     }
     
